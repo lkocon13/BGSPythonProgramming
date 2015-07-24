a4c3aab2cee4de207f9718803d1ab69bdb60cd5e
Daniel Ferguson <danielf@bgs.vic.edu.au>
2015-07-24 22:19:32 +1000

    Friday, 24th July 2015 Just committing a few more Python programs. Updated to Chapter 4

---
+++ Ferg's Program Files/birthdays.py
@@ -0,0 +1,16 @@
+➊ birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+
+   while True:
+       print('Enter a name: (blank to quit)')
+       name = input()
+       if name == '':
+           break
+
+➋     if name in birthdays:
+➌         print(birthdays[name] + ' is the birthday of ' + name)
+       else:
+           print('I do not have birthday information for ' + name)
+           print('What is their birthday?')
+           bday = input()
+➍         birthdays[name] = bday
+           print('Birthday database updated.')
---
+++ Ferg's Program Files/listsTheRightAndWrongWay.py
@@ -0,0 +1,31 @@
+# Lists
+
+
+# This is the wrong way to program a group of variables...
+print('Enter the name of cat 1:')
+catName1 = input()
+print('Enter the name of cat 2:')
+catName2 = input()
+print('Enter the name of cat 3:')
+catName3 = input()
+print('Enter the name of cat 4:')
+catName4 = input()
+print('Enter the name of cat 5:')
+catName5 = input()
+print('Enter the name of cat 6:')
+catName6 = input()
+print('The cat names are:')
+print(catName1 + ' ' + catName2 + ' ' + catName3 + ' ' + catName4 + ' ' + catName5 + ' ' + catName6)
+
+# This is the right way to do a list for a group of variables...
+catNames = []
+while True:
+    print('Enter the name of cat ' + str(len(catNames) + 1) +
+      ' (Or enter nothing to stop.):')
+    name = input()
+    if name == '':
+        break
+    catNames = catNames + [name] # list concatenation
+print('The cat names are:')
+for name in catNames:
+    print('  ' + name)
